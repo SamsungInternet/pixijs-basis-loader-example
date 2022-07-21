45a8411e93a3b9a3aab0af9a1301886919a6d56a
Sylwester Mielniczuk <flaboy.com@gmail.com>
2022-07-21 14:36:31 +0100

    BasisLoader.loadTranscoder - promise resolve

--- src/index.js
+++ src/index.js
@@ -1,64 +1,71 @@
-console.log("index.js");
+console.log("%c Hi, all! - PixiJS BasisLoader Example", 'background: #ff0000; color: #ffff; padding: 0.3rem');
+
 
 import css from './style.css';
 import { Application, Loader, Sprite } from 'pixi.js'
 import { BasisLoader } from '@pixi/basis';
 Loader.registerPlugin(BasisLoader);
 
-BasisLoader.loadTranscoder('./assets/basis_transcoder.js', './assets/basis_transcoder.wasm');
 
 let app;
 const sprites = [];
+let time = {};
+time.start = new Date().getTime()
 
+function timeLeft() {
+    return "| time >> "+(new Date().getTime() - time.start)+"ms";
+}
+
+
+function basisInit() {
+    console.log("basisInit", timeLeft())
+    const check = BasisLoader.loadTranscoder('./assets/basis_transcoder.js', './assets/basis_transcoder.wasm')
+    check.then(function() {
+        console.log("%c BasisLoader, promise solved "+timeLeft(), 'background: #222; color: #bada55; padding: 0.3rem');
+        initPixi();
+    })
+}
 
 function loadAssets() {
-
+    console.log("loadAssets", timeLeft())
     Loader.shared
         .add('shape01', 'assets/textures/300/opt/01.basis')
-
     Loader.shared.load((loader, resources) => {
         console.log("loader load");
     });
-
-    Loader.shared.onProgress.add(() => {
-        console.log("onProgress")
+    Loader.shared.onProgress.add((e) => {
+        console.log("onProgress", e);
     });
     Loader.shared.onError.add(() => {
-        console.log("onError")        
+        console.log("onError");
     });
     Loader.shared.onLoad.add(() => {
-        console.log("onLoad")
+        console.log("onLoad");
     });
-
     Loader.shared.onComplete.once(() => {
         const resources = Loader.shared.resources;
         addSprites(resources);
     });
 }
 
-
 function addSprites(resources) {
-
-    const planet = Sprite.from(resources["shape01"].texture);
-
+    console.log("addSprites, resources", resources, timeLeft());
+    let texture = resources["shape01"].texture;
+    console.log(texture.orig.width, texture.orig.height, texture)
+    const planet = Sprite.from(texture);
     planet.anchor.set(0.5);
-
     planet.x = app.screen.width / 2;
     planet.y = app.screen.height / 2;
     planet.scale.set(1)
-
     app.stage.addChild(planet);
-
-
     sprites.push(planet)
-
     app.ticker.add((delta) => {
         planet.rotation += 0.01 * delta;
     });
 }
 
-
 function initPixi() {
+    console.log("initPixi", timeLeft())
     let view = document.getElementById("screen")
     app = new Application({
         backgroundColor: 0x132CAD,
@@ -70,12 +77,13 @@ function initPixi() {
     loadAssets();
 }
 
-
 function resizeApp() {
+    console.log("resizeApp", timeLeft())
     console.log($(window).width(), $(window).height());
 }
 
+basisInit()
 
-initPixi();
 
+// setTimeout(initPixi,500);
 window.addEventListener('resize', resizeApp, false);
